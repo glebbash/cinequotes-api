51e5d33a64f5c8489aae6c33d74adced3eb12e7c
glebbash <glebbash@gmail.com>
2020-12-02 16:19:27 +0100

    Fix pubsub connection options

--- src/pub-sub/pub-sub.module.ts
+++ src/pub-sub/pub-sub.module.ts
@@ -1,15 +1,13 @@
 import { GcloudPubSubModule } from '@ecobee/nodejs-gcloud-pubsub-module'
 import { Module } from '@nestjs/common'
 import { PubSubService } from './pub-sub.service'
-import * as grpc from '@grpc/grpc-js'
 
 @Module({
     imports: [
         GcloudPubSubModule.forRoot({
             authOptions: {
                 projectId: 'dummy',
-                uri: 'localhost:8085',
-                credentials: grpc.credentials.createInsecure(),
+                uri: process.env.PUBSUB_EMULATOR_HOST,
             },
         }),
     ],

935d33f301079e85e9e49f059da15f1c3611e29e
glebbash <glebbash@gmail.com>
2020-12-02 16:20:44 +0100

    Refactor imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common'
-import { PubSubModule } from './pub-sub/pub-sub.module'
-import { QuotesModule } from './quotes/quotes.module'
-import { FilmsModule } from './films/films.module'
-import { FirestoreModule } from './firestore/firestore.module'
+import { PubSubModule } from '@/pub-sub/pub-sub.module'
+import { QuotesModule } from '@/quotes/quotes.module'
+import { FilmsModule } from '@/films/films.module'
+import { FirestoreModule } from '@/firestore/firestore.module'
 
 @Module({
     imports: [PubSubModule, QuotesModule, FirestoreModule, FilmsModule],

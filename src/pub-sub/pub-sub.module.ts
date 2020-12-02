cc6e93f88a6f0f65a07cb950c96e9a7569090cf5
glebbash <glebbash@gmail.com>
2020-12-02 13:24:08 +0100

    Configure pub-sub module

--- src/pub-sub/pub-sub.module.ts
+++ src/pub-sub/pub-sub.module.ts
@@ -1,7 +1,19 @@
+import { GcloudPubSubModule } from '@ecobee/nodejs-gcloud-pubsub-module'
 import { Module } from '@nestjs/common'
 import { PubSubService } from './pub-sub.service'
+import * as grpc from '@grpc/grpc-js'
 
 @Module({
+    imports: [
+        GcloudPubSubModule.forRoot({
+            authOptions: {
+                projectId: 'dummy',
+                uri: 'localhost:8085',
+                credentials: grpc.credentials.createInsecure(),
+            },
+        }),
+    ],
     providers: [PubSubService],
+    exports: [PubSubService],
 })
 export class PubSubModule {}

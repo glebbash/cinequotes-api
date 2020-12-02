905df71937579e6f34cafd0b0bd9057f85339f3d
glebbash <glebbash@gmail.com>
2020-12-02 16:18:06 +0100

    Add firestore tests

--- src/firestore/firestore.service.spec.ts
+++ src/firestore/firestore.service.spec.ts
@@ -1,18 +1,19 @@
-import { Test, TestingModule } from '@nestjs/testing'
 import { FirestoreService } from './firestore.service'
+
+jest.setTimeout(70000)
 
 describe('FirestoreService', () => {
     let service: FirestoreService
 
     beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [FirestoreService],
-        }).compile()
-
-        service = module.get<FirestoreService>(FirestoreService)
+        service = new FirestoreService()
     })
 
     it('should be defined', () => {
         expect(service).toBeDefined()
+    })
+
+    it('should connect to emulator', async () => {
+        await service.db.collection('films').get()
     })
 })

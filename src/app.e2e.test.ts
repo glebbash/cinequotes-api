2cf435240146cdeb87e07b819f998b665c08f770
glebbash <glebbash@gmail.com>
2020-12-03 18:31:08 +0100

    Update e2e test

--- src/app.e2e.test.ts
+++ src/app.e2e.test.ts
@@ -1,20 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import { AppModule } from '@/app.module'
+import { FirestoreService } from '@/firestore/firestore.service'
+import { FS_FILMS_COL } from './common/constants'
+import { GcloudPubSubService } from '@ecobee/nodejs-gcloud-pubsub-module'
 
 describe('AppController (e2e)', () => {
     let app: INestApplication
 
     beforeEach(async () => {
-        const moduleFixture: TestingModule = await Test.createTestingModule({
+        const module: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
         }).compile()
 
-        app = moduleFixture.createNestApplication()
+        app = module.createNestApplication()
         await app.init()
     })
 
-    it('should be defined', () => {
-        expect(app).toBeDefined()
+    it('connects to firestore', async () => {
+        const firestore = app.get(FirestoreService)
+
+        const res = await firestore.db.collection(FS_FILMS_COL).limit(1).get()
+
+        expect(res).toBeDefined()
+    })
+
+    it('connects to pubsub', async () => {
+        const pubsub = app.get(GcloudPubSubService)
+
+        const topic = await pubsub.gcloudPubSubLib.getTopics({ pageSize: 1 })
+
+        expect(topic).toBeDefined()
     })
 })

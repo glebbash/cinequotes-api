b75b0b54aa950f584c72a30515359188315c3d98
glebbash <glebbash@gmail.com>
2020-12-03 17:25:14 +0100

    Add tests to pubsub module

--- src/pub-sub/pub-sub.service.spec.ts
+++ src/pub-sub/pub-sub.service.spec.ts
@@ -1,27 +1,38 @@
-import { GcloudPubSubModule } from '@ecobee/nodejs-gcloud-pubsub-module'
+import { PUBSUB_TOPIC } from '@/common/constants'
+import { GcloudPubSubService } from '@ecobee/nodejs-gcloud-pubsub-module'
 import { Test, TestingModule } from '@nestjs/testing'
 import { PubSubService } from './pub-sub.service'
+import * as faker from 'faker'
+
+jest.mock('@ecobee/nodejs-gcloud-pubsub-module')
 
 describe('PubSubService', () => {
+    let pubsub: GcloudPubSubService
     let service: PubSubService
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [
-                GcloudPubSubModule.forRoot({
-                    authOptions: {
-                        projectId: 'dummy',
-                        uri: process.env.PUBSUB_EMULATOR_HOST,
-                    },
-                }),
-            ],
-            providers: [PubSubService],
+            providers: [GcloudPubSubService, PubSubService],
         }).compile()
 
-        service = module.get<PubSubService>(PubSubService)
+        service = module.get(PubSubService)
+        pubsub = module.get(GcloudPubSubService)
     })
 
-    it('should be defined', () => {
-        expect(service).toBeDefined()
+    it('publishes stringified messages', async () => {
+        const message = {
+            actor: faker.name.findName(),
+        }
+        const json = JSON.stringify(message)
+        const mockResult = 'result'
+
+        const publishSpy = jest
+            .spyOn(pubsub, 'publishMessage')
+            .mockResolvedValue(mockResult)
+
+        const result = await service.publish(message)
+
+        expect(publishSpy).toBeCalledWith(PUBSUB_TOPIC, json)
+        expect(result).toEqual(mockResult)
     })
 })

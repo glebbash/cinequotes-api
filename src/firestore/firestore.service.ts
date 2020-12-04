bd8fd2d6439e852f96eab69d7d5afb29339b60d7
glebbash <glebbash@gmail.com>
2020-12-04 15:04:07 +0100

    Add missed @Injectable in FirestoreService

--- src/firestore/firestore.service.ts
+++ src/firestore/firestore.service.ts
@@ -1,5 +1,7 @@
 import { Firestore } from '@google-cloud/firestore'
+import { Injectable } from '@nestjs/common'
 
+@Injectable()
 export class FirestoreService {
     db = new Firestore({
         projectId: process.env.FIRESTORE_PROJECT_ID ?? 'dummy-test',
